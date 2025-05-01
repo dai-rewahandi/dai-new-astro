e4af78e0cf6227650652718e3aeee01b49e83342
dai-rewahandi <niktodaire@gmail.com>
2025-05-01 12:36:18 +0700

    feat: add global styles, pages, and components for project
    Add global CSS styles, project page, and various components including Header, Footer, and Layout. Configure TailwindCSS, Astro integrations, and TypeScript paths. Update package.json with necessary dependencies and devDependencies. Introduce new pages for skills and work details, and enhance the index page with dynamic content and transitions.

---
+++ src/lib/var.ts
@@ -0,0 +1,10 @@
+export const User: { name: string, url: string, desc: string} = {
+  name: "Dai.r",
+  url: "/",
+  desc: "lorem ipsum dolor sit amet consectetur adipiscing elit."
+}
+
+export const Links: { name: string, url: string}[] = [
+  { name: "Home", url: "/" },
+  { name: "Project", url: "/project"}
+]
